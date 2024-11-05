7283d0eb24cc1a03cdf3811afbed2200c4993a85
Alejandro Sáez <alejandro.sapa@gmail.com>
2024-11-05 17:28:36 -0300

    change umami code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-    <script async src="https://analytics.umami.is/script.js" data-website-id="dacf57bf-9a0d-4fb0-a470-b9562dadf76f"></script>
+    <script defer src="https://umami-production-da63.up.railway.app/script.js" data-website-id="c0962a01-1119-4dc2-a9fa-707b85c107ff"></script>
 </svelte:head>
 
 <slot/>
